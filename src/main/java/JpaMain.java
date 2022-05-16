bbba204ee0069cefb7cdde8aad5ad6d9ba85a3cf
minjun <mjk54565@gmail.com>
2022-05-17 00:30:22 +0900

    엔티티 CRUD 예제

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,12 +1,18 @@
+import domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //JPA 사용을 위해 persistent.xml의 설정 정보를 이용해 엔티티 매니저 팩토리 생성(비용이 크므로 한번만 생성하고 공유). 애플리케이션이 끝날 때 close 호출.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        //엔티티 매니저 생성. 이를 이용해 엔티티를 데이터베이스에 CRUD 가능. 스레드간 공유나 재사용 불가. 사용이 끝나면 close 호출.
         EntityManager em = emf.createEntityManager();
+        //트랜잭션 생성
         EntityTransaction tx = em.getTransaction();
 
         try{
@@ -23,6 +29,21 @@ public class JpaMain {
     }
 
     public static void logic(EntityManager em){
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("길동");
+        member.setAge(10);
 
+        em.persist(member);
+        member.setAge(20);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println(findMember);
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println(members.size());
+
+        em.remove(member);
     }
 }
